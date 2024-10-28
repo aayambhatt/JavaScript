1bf2003aaab6f1efbb58499e81cc358d20c99e5d
aayambhatt <aayambhatt23@gmail.com>
2024-10-28 23:44:09 +0530

    files added

---
+++ 03-window-this-keyword/script.js
@@ -0,0 +1,7 @@
+// window is a global object created along global execution  context 
+// this object is also created that points to window 
+// JS not only runs on browser, it also runs on server, like on chrome its V8 engine and other browser have diff engines 
+//in case of browser, it is known as window 
+// at this global level, "this==window" is true 
+// Anything which is not inside any function is the global space 
+// if we create any variable and function over in global space, they get attached to window 
---
+++ 04-Undefined-Not-Defined/script.js
@@ -0,0 +1,5 @@
+// undefined does not mean empty, its a placeholder which is kept for the timebeing, until the variable is assigned any other value 
+console.log(a); // outputs undefined 
+var a = 7; // memory space to a is allocated, that is undefined
+console.log(a); // outputs 7 
+console.log(x); // ReferenceError: x is not defined
